c57854f30c96e05a618f97a88995c59c1a8f9b05
backmandu <bmj3222@naver.com>
2023-04-05 11:36:45 +0900

    25

--- Untitled-1.py
+++ Untitled-1.py
@@ -25,5 +25,5 @@ my_new_car = Car('audi', 'a4')
 
 print(my_new_car.get_descriptive_name())  # Fixed method call
 
-my_new_car.update_odometer(23)
+my_new_car.update_odometer(25)
 my_new_car.read_odometer()
